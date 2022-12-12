059ea51d17da7d6c8b2a09680efa93932e8732bc
feindreh <119032218+feindreh@users.noreply.github.com>
2022-12-12 17:06:40 +0100

    add basic button layout

--- script.js
+++ script.js
@@ -10,23 +10,30 @@ function multi(num1,num2){
     return num1*num2
 }
 
-function divid(num1,num2){
+function divide(num1,num2){
     return num1/num2
 }
 
 function operate(operator,num1,num2){
     switch(operator){
         case "plus":
+            console.log(add(num1,num2));
             return add(num1,num2);
             break;
         case "minus":
+            console.log(sub(num1,num2));
             return sub(num1,num2);
             break;
         case "multiply":
+            console.log(multi(num1,num2));
             return multi(num1,num2);
             break;
         case "divide":
+            console.log(divide(num1,num2));
             return divide(num1,num2);
             break;
     }
 }
+
+
+
