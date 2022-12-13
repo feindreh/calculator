ce2e8bf935fee1b30449bf2eae40d4d795b7daf1
feindreh <119032218+feindreh@users.noreply.github.com>
2022-12-14 00:42:42 +0100

    make clear work

--- script.js
+++ script.js
@@ -36,7 +36,15 @@ function operate(operator,num1,num2){
 }
 
 function addToDisplay(num){
-    display.textContent = num
+    if(cleared === 1){cleared = 0; displayed = "";}
+    displayed = displayed + `${num}`
+    display.textContent = displayed
+}
+
+function deleteDisplay(){
+    displayed = "calculator"
+    cleared = 1
+    display.textContent = displayed
 }
 
 
@@ -54,8 +62,6 @@ const zero = document.querySelector("#zero")
 const display = document.querySelector("#display")
 const clear = document.querySelector("#clear")
 
-
-
 one.addEventListener('click', () => addToDisplay(1))
 two.addEventListener('click', () => addToDisplay(2))
 three.addEventListener('click', () => addToDisplay(3))
@@ -66,5 +72,17 @@ seven.addEventListener('click', () => addToDisplay(7))
 eight.addEventListener('click', () => addToDisplay(8))
 nine.addEventListener('click', () => addToDisplay(9))
 zero.addEventListener('click', () => addToDisplay(0))
+clear.addEventListener('click', () => deleteDisplay())
+
+
+// content of #display
+let displayed = ""
+
+//variable to store the information if the last 
+//button was the #clear button 
+// delete display sets it to 1
+// any other key sets it to 0
+
+let cleared = 1
 
 
